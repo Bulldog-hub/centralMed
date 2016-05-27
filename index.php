d2e27b204ffe3e5fc63f76bd0df7fa21427a376e
BullTheDog <o.plessis@lacentralepharma.com>
2016-05-28 00:27:02 +0200

    push

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Passport</a></li>
+                        <li><a href="#">Equipe</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                 </div>
