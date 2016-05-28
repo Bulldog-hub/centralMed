ac1f994eeb30b2c0b820ac339385ba976884f784
Igor Descayrac <igor.descayrac@gmail.com>
2016-05-28 23:21:03 +0200

    Use bootstrap panels

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,50 +1,92 @@
 <?php include "header.php" ?>
         <!-- Page Content -->
-
+        <div id="page-content-wrapper">
             <div class="container-fluid xyz">
                 <div class="rubrique row" id="dashboard" >
                     <div class="row">
-                    <div class="col-sm-8">
-                        <div class="widget">
-                            <div class="widget_title">
+                        <div class="col-sm-8">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
                                 <i class="fa fa-external-link-square"></i>
-                                <span class="title"> Pathologie en cours </span>
+                                <span class="title"> Pathologies en cours </span>
                                 <i class="fa fa-cog"></i>
                             </div>
-                            <ul class="table">
-                                <li><strong>Hyperthyroidie d'Hashimoto</strong> <br>
-                                (Baisse de la fonction de la thyroide liée au dysfonctionnement du système immunitaire)</li>
-                                <li><strong>Diabète de type 2</strong></li>
-                            </ul>
-                        </div>
-                    
-                        <div class="widget">
-                            <div class="widget_title">
-                                <i class="fa fa-external-link-square"></i>
-                                <span class="title"> Antécédent médical </span>
-                                <i class="fa fa-cog"></i>
+                            <div class="panel-body">
+                                <ul>
+                                    <li>
+                                        <strong>Hyperthyroidie d'Hashimoto</strong>
+                                        (Baisse de la fonction de la thyroide liée au dysfonctionnement du système immunitaire)
+                                        <a href="#" class="disease-info" data-toggle="modal" data-target="#hashimotoModal">
+                                            <i class="fa fa-info-circle"></i>
+                                            <div class="modal fade" id="hashimotoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                            <h4 class="modal-title">Hyperthyroidie d'Hashimoto</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            La thyroïdite de Hashimoto ou thyroïdite chronique lymphocytaire est une thyroïdite chronique auto-immune. La glande thyroïde est attaquée par des processus immuns impliquant une variété de cellules et d'anticorps. C'est la première maladie à avoir été reconnue comme maladie auto-immune. Elle a été décrite pour la première fois par le médecin japonais Hakaru Hashimoto en Allemagne en 1912.
+                                                        </div>
+                                                    </div><!-- /.modal-content -->
+                                                </div><!-- /.modal-dialog -->
+                                            </div><!-- /.modal -->
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <strong>Diabète de type 2</strong>
+                                        <a href="#" class="disease-info" data-toggle="modal" data-target="#diabeteModal">
+                                            <i class="fa fa-info-circle"></i>
+                                            <div class="modal fade" id="diabeteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                            <h4 class="modal-title">Diabète de type 2</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Le « diabète de type 2 » ou « diabète non insulinodépendant » (DNID) (aussi appelé « diabète insulinorésistant » ou « diabète de l'âge mûr », parfois « diabète acquis »), est une maladie métabolique touchant la glycorégulation provoquant à terme un diabète sucré.
+                                                        </div>
+                                                    </div><!-- /.modal-content -->
+                                                </div><!-- /.modal-dialog -->
+                                            </div><!-- /.modal -->
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
-                            <ul class="table">
-                            <li>Pneumopathie</li>
-                            <li>Appendicectomie</li>
-                            <li>Varicelle d'Arthur Février 2016</li>
-                            </ul>
                         </div>
-                    </div>
 
-                    <div class="widget col-sm-4">
-                        <div class="widget_title">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <i class="fa fa-external-link-square"></i>
+                                <span class="title"> Antécédents médicaux </span>
+                                <i class="fa fa-cog"></i>
+                            </div>
+                            <div class="panel-body">
+                                <ul>
+                                    <li>Pneumopathie</li>
+                                    <li>Appendicectomie</li>
+                                    <li>Varicelle d'Arthur Février 2016</li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+
+                        <div class="col-sm-4">
+                        <div class="panel panel-primary">
+                        <div class="panel-heading">
                             <i class="fa fa-external-link-square"></i>
                             <span class="title"> Agenda médical </span>
                             <i class="fa fa-cog"></i>
                         </div>
+                        <div class="panel-body">
                         <ul class="tabs">
                             <li class="active">Personnel</li>
                             <li>Famille</li>
                         </ul>
 
                             <ul class="tab__content ">
-                                <li class="active">
+                                <li class="active englobing">
                                     <ul class="agenda table list-group">
                                         <li class="list-group-item list-group-item-danger">Renouvellement médication <span class="badge">30 mai 2016</span></li>
                                         <li class="list-group-item list-group-item-danger">Prise de sang <span class="badge">2 juin 2016</span></li>
@@ -52,41 +94,57 @@
                                         <li class="list-group-item list-group-item-info">RDV DR Bammert <span class="badge">16 juin 2016</span></li>
                                     </ul>
                                 </li>
-                                <li>
+                                <li class="englobing">
                                     <ul class="agenda table list-group">
-                                       <li class="list-group-item list-group-item-danger">Vaccin arthur <span class="badge">2 Juin 2016</span></li>
+                                        <li class="list-group-item list-group-item-danger">Vaccin arthur <span class="badge">2 Juin 2016</span></li>
                                         <li class="list-group-item list-group-item-danger">Prise de sang <span class="badge">2 juin 2016</span></li>
 
-                                    </ul>
-                                </li>
-                            </ul>
+                                 </ul>
+                             </li>
+                         </ul>
+
                     </div>
                     </div>
-                    
+                    </div>
+                 </div>
+
                     <div class="row">
-                    <div class="widget col-sm-4">
-                        <div class="widget_title">
-                            <i class="fa fa-external-link-square"></i>
-                            <span class="title"> Documents </span>
-                            <i class="fa fa-cog"></i>
+                        <div class="col-sm-4">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <i class="fa fa-external-link-square"></i>
+                                    <span class="title"> Documents </span>
+                                    <i class="fa fa-cog"></i>
+                                </div>
+                                <div class="panel-body">
+                                    <ul>
+                                        <li>
+                                            <a href="?documents#radio_pulmonaire">Radio pulmonaire <span class="badge">Avril 2015</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="?documents#analyse_biologique">Analyse biologique <span class="badge">Mai 2015</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="?documents#radio_pulmonaire">Radio pulmonaire <span class="badge">Avril 2015</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="?documents#analyse_biologique">Analyse biologique <span class="badge">Mai 2015</span></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <ul class="table">
-                        <li>Radio pulmonaire <span class="date">Avril 2015</span></li>
-                        <li>Analyse biologique <span class="date">Mai 2015</span></li>
-                        <li>Radio pulmonaire <span class="date">Avril 2015</span></li>
-                        <li>Analyse biologique <span class="date">Mai 2015</span></li>
-                        </ul>
-                    </div>
 
-                    <div class="widget col-sm-8">
-                        <div class="widget_title">
-                            <i class="fa fa-external-link-square"></i>
-                            <span class="title"> RDV sur l'année </span>
-                            <i class="fa fa-cog"></i>
-                            <img class="img-responsive" src="img/stats.png" alt="">
+                        <div class="col-sm-8">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <i class="fa fa-external-link-square"></i>
+                                    <span class="title"> RDV sur l'année </span>
+                                    <i class="fa fa-cog"></i>
+                                    <img class="img-responsive" src="img/stats.png" alt="">
+                                </div>
+                            </div>
                         </div>
-
-                    </div>
                     </div>
                 </div>
                 <div class="rubrique" id="pathologie" style="display: none;"></div>
@@ -134,7 +192,8 @@
                                 </div>
                             </div>
             </div>
-     
+        </div>
+        <!-- /#page-content-wrapper -->
 
 
 <?php include "footer.php" ?>
